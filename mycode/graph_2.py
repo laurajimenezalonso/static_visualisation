48eac3b393424344e7ee7dea2ad3aaa37b0b2c9a
laurajimenezalonso <laura.jimenez.alonso@gmail.com>
2025-09-11 20:09:45 +0100

    Exercise 2

--- mycode/graph_2.py
+++ mycode/graph_2.py
@@ -9,27 +9,19 @@ data = pd.read_csv('../data/cleanSalaries.csv')
 
 
 data_ej2=(
-    data.groupby(['fixed_country'])[['total_salary_usd']]
+    data.groupby(['fixed_country','currency'])[['total_salary_usd']]
     .mean()
     .reset_index()
     .sort_values(by='total_salary_usd', ascending=False)
     .head(8)
 )
 
-#usando matplotlib
-plt.bar(data_ej2.fixed_country, data_ej2.total_salary_usd)
-plt.title('Salario medio por divisa')
-plt.xlabel('Pais')
-plt.ylabel('Salario medio ($)')
-plt.yticks(range(0, 200000, 25000), 
-           labels=[f"${number//1000:,.0f}K" for number in range(0, 200000, 25000)])
 
-plt.show()
 
 #usando plotnine
 (
-    ggplot(data_ej2, aes(x='fixed_country', y='total_salary_usd')) +
-    geom_bar(stat='identity', fill='blue') +
+    ggplot(data_ej2, aes(x='fixed_country', y='total_salary_usd', fill='currency')) +
+    geom_bar(stat='identity') +
     ggtitle('Salario medio por divisa') +
     xlab('País') +
     ylab('Salario medio ($)') +
@@ -37,6 +29,40 @@ plt.show()
     
 )
 
+
+#usando matplotlib
+
+
+# Crear un diccionario que asigne un color a cada divisa
+currencies = data_ej2['currency'].unique()
+len(currencies)
+#vemos que hay 5 divisas. Podriamos crear una lista de colores mayor
+colors = ['indianred','mediumseagreen','cornflowerblue','gold','violet']  # paleta de colores
+color_map = {cur: colors[i % len(colors)] for i, cur in enumerate(currencies)}
+
+# Asignar colores a cada fila según su divisa
+bar_colors = data_ej2['currency'].map(color_map)
+
+plt.bar(data_ej2.fixed_country, data_ej2.total_salary_usd, color=bar_colors)
+plt.title('Salario medio por divisa')
+plt.xlabel('Pais')
+plt.xticks(fontsize=8)
+plt.ylabel('Salario medio ($)')
+plt.yticks(range(0, 200000, 25000), 
+           labels=[f"${number//1000:,.0f}K" for number in range(0, 200000, 25000)])
+
+# Leyenda directa desde el diccionario
+from matplotlib.patches import Patch
+legend_handles = [Patch(color=c, label=k) for k, c in color_map.items()]
+plt.legend(handles=legend_handles, 
+        title="Divisa",
+        loc="upper center",       # punto de referencia en el centro superior
+        bbox_to_anchor=(0.5, -0.1), 
+        ncol=5,
+        frameon=False)
+
+plt.tight_layout()
+plt.show()
 
 
 
